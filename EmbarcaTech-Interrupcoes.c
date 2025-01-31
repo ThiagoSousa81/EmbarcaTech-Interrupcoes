314b7748cfe691519535416426365d0415090bb7
Thiago Sousa <thiagosousa81@gmail.com>
2025-01-30 21:16:26 -0300

    § compilação §

--- EmbarcaTech-Interrupcoes.c
+++ EmbarcaTech-Interrupcoes.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include <stdio.h>                   // Biblioteca padrão de entrada e saída
+#include "pico/stdlib.h"            // Biblioteca padrão para o Raspberry Pi Pico
+#include <hardware/pio.h>           // Biblioteca para manipulação de periféricos PIO
+#include "hardware/clocks.h"        // Biblioteca para controle de relógios do hardware
+#include "ws2818b.pio.h"            // Biblioteca PIO para controle de LEDs WS2818B
 
 int main()
 {
