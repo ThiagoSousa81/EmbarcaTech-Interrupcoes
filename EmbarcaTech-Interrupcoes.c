6f14ce6afb33982448b09cea4496042de99eceb9
Thiago Sousa <thiagosousa81@gmail.com>
2025-01-30 22:48:09 -0300

    § nums §

--- EmbarcaTech-Interrupcoes.c
+++ EmbarcaTech-Interrupcoes.c
@@ -70,24 +70,189 @@ void npClear()
     for (uint i = 0; i < LED_COUNT; ++i)                  // Iterar sobre todos os LEDs
         npSetLED(i, 0, 0, 0);                             // Definir cor como preta (apagado)
         
-    npUpdate();
+    //npUpdate();
 }
 
 void setDisplayNum(int num, const uint8_t r, const uint8_t g, const uint8_t b)
 {
+
+    /*
+    24, 23, 22, 21, 20
+    15, 16, 17, 18, 19
+    14, 13, 12, 11, 10
+    05, 06, 07, 08, 09
+    04, 03, 02, 01, 00
+    */
     npClear();
-    switch (num)
-    {
-        case 1:
+    switch (num) {
+        case 0:
+            npSetLED(0, r, g, b);
             npSetLED(1, r, g, b);
             npSetLED(2, r, g, b);
             npSetLED(3, r, g, b);
+            npSetLED(4, r, g, b);
+            npSetLED(5, r, g, b);          
+            npSetLED(9, r, g, b);
+            npSetLED(10, r, g, b);            
+            npSetLED(14, r, g, b);
+            npSetLED(15, r, g, b);
+            npSetLED(19, r, g, b);
+            npSetLED(20, r, g, b);
+            npSetLED(21, r, g, b);
+            npSetLED(22, r, g, b);
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;  
+        case 1:
+            npSetLED(0, r, g, b);
+            npSetLED(1, r, g, b);
+            npSetLED(2, r, g, b);
+            npSetLED(3, r, g, b);
+            npSetLED(4, r, g, b);
             npSetLED(7, r, g, b);
             npSetLED(12, r, g, b);
             npSetLED(16, r, g, b);
             npSetLED(17, r, g, b);
             npSetLED(22, r, g, b);
-            break;        
+            break;  
+        case 2:
+            npSetLED(0, r, g, b);
+            npSetLED(1, r, g, b);
+            npSetLED(2, r, g, b);
+            npSetLED(3, r, g, b);
+            npSetLED(4, r, g, b);
+            npSetLED(5, r, g, b);
+            npSetLED(10, r, g, b);
+            npSetLED(11, r, g, b);
+            npSetLED(12, r, g, b);
+            npSetLED(13, r, g, b);
+            npSetLED(14, r, g, b);
+            npSetLED(19, r, g, b);
+            npSetLED(20, r, g, b);
+            npSetLED(21, r, g, b);
+            npSetLED(22, r, g, b);
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
+        case 3:
+            npSetLED(0, r, g, b);
+            npSetLED(1, r, g, b);
+            npSetLED(2, r, g, b);
+            npSetLED(3, r, g, b);
+            npSetLED(4, r, g, b); 
+            npSetLED(9, r, g, b);           
+            npSetLED(10, r, g, b);
+            npSetLED(11, r, g, b);
+            npSetLED(12, r, g, b);
+            npSetLED(13, r, g, b);
+            npSetLED(19, r, g, b);
+            npSetLED(20, r, g, b);
+            npSetLED(21, r, g, b);
+            npSetLED(22, r, g, b);
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
+        case 4:
+            npSetLED(2, r, g, b);   
+            npSetLED(5, r, g, b);
+            npSetLED(6, r, g, b);   
+            npSetLED(7, r, g, b);   
+            npSetLED(8, r, g, b);   
+            npSetLED(12, r, g, b);
+            npSetLED(14, r, g, b);  
+            npSetLED(16, r, g, b);  
+            npSetLED(17, r, g, b);  
+            npSetLED(22, r, g, b);  
+            break;
+        case 5:
+            npSetLED(0, r, g, b);   // LED na posição (0, 0)
+            npSetLED(1, r, g, b);   // LED na posição (0, 1)
+            npSetLED(2, r, g, b);   // LED na posição (0, 2)
+            npSetLED(3, r, g, b);   // LED na posição (0, 3)
+            npSetLED(4, r, g, b);   // LED na posição (0, 4)
+            npSetLED(9, r, g, b);
+            npSetLED(10, r, g, b);  // LED na posição (2, 0)
+            npSetLED(11, r, g, b);  // LED na posição (2, 1)
+            npSetLED(12, r, g, b);  // LED na posição (2, 2)
+            npSetLED(13, r, g, b);  // LED na posição (2, 3)
+            npSetLED(14, r, g, b);  // LED na posição (3, 4)
+            npSetLED(15, r, g, b);  // LED na posição (4, 0)
+            npSetLED(20, r, g, b);
+            npSetLED(21, r, g, b);  // LED na posição (4, 1)
+            npSetLED(22, r, g, b);  // LED na posição (4, 2)
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
+        case 6:
+            npSetLED(0, r, g, b);   
+            npSetLED(1, r, g, b);   
+            npSetLED(2, r, g, b);   
+            npSetLED(3, r, g, b);   
+            npSetLED(4, r, g, b);  
+            npSetLED(5, r, g, b);                 
+            npSetLED(9, r, g, b);   
+            npSetLED(10, r, g, b);  
+            npSetLED(11, r, g, b);  
+            npSetLED(12, r, g, b);  
+            npSetLED(13, r, g, b);  
+            npSetLED(14, r, g, b);  
+            npSetLED(15, r, g, b);  
+            npSetLED(20, r, g, b); 
+            npSetLED(21, r, g, b);  
+            npSetLED(22, r, g, b);  
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
+        case 7:            
+            npSetLED(1, r, g, b);   
+            npSetLED(8, r, g, b);   
+            npSetLED(11, r, g, b);  
+            npSetLED(18, r, g, b);  
+            npSetLED(21, r, g, b);  
+            npSetLED(22, r, g, b);
+            npSetLED(23, r, g, b);
+            break;
+        case 8:
+            npSetLED(0, r, g, b);  
+            npSetLED(1, r, g, b); 
+            npSetLED(2, r, g, b); 
+            npSetLED(3, r, g, b);  
+            npSetLED(4, r, g, b);  
+            npSetLED(5, r, g, b);  
+            npSetLED(9, r, g, b);  
+            npSetLED(10, r, g, b); 
+            npSetLED(11, r, g, b);
+            npSetLED(12, r, g, b); 
+            npSetLED(13, r, g, b); 
+            npSetLED(14, r, g, b); 
+            npSetLED(15, r, g, b);
+            npSetLED(19, r, g, b); 
+            npSetLED(20, r, g, b); 
+            npSetLED(22, r, g, b); 
+            npSetLED(21, r, g, b);  
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
+        case 9:
+            npSetLED(0, r, g, b);   
+            npSetLED(1, r, g, b);   
+            npSetLED(2, r, g, b);   
+            npSetLED(3, r, g, b);   
+            npSetLED(4, r, g, b);   
+            npSetLED(9, r, g, b);  
+            npSetLED(10, r, g, b);  
+            npSetLED(11, r, g, b);  
+            npSetLED(12, r, g, b);  
+            npSetLED(13, r, g, b);  
+            npSetLED(14, r, g, b);
+            npSetLED(15, r, g, b);
+            npSetLED(19, r, g, b);  
+            npSetLED(20, r, g, b);  
+            npSetLED(21, r, g, b);
+            npSetLED(22, r, g, b);
+            npSetLED(23, r, g, b);
+            npSetLED(24, r, g, b);
+            break;
     }
     npUpdate();
 }
@@ -96,14 +261,13 @@ int main()
 {
     stdio_init_all();                                     // Inicializar a comunicação serial
     npInit(LED_PIN);                                      // Inicializar os LEDs
-    npClear();                                            // Apagar todos os LEDs    
 
     while (true) {
-        npClear();                                 // Limpar todos os LEDs
-        sleep_ms(1000);
-
-        npSetLED(12, 0, 255, 0);                    // Acender o LED correspondente (verde)        
-        npUpdate();
-        sleep_ms(1000);
+        for (uint i = 0; i < 10; ++i)
+        {
+            setDisplayNum(i, 0, 255, 255);        
+            sleep_ms(1000);
+        }
+        
     }
 }
